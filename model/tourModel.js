e5b954fe5d41741c5b32d0161e179e445126eee0
Nguyễn Đình Tú <tu700131@gmail.com>
2025-01-31 16:56:02 +0700

    modify tour model

--- model/tourModel.js
+++ model/tourModel.js
@@ -75,7 +75,17 @@ const tourSchema = new mongoose.Schema({
     secretTour: {
         type: Boolean,
         default: false
+    },
+
+},
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true }
     }
+);
+
+tourSchema.virtual('durationWeeks').get(function () {
+    return this.duration / 7;
 });
 
 tourSchema.pre('save', function (next) {
@@ -85,6 +95,13 @@ tourSchema.pre('save', function (next) {
 
 tourSchema.pre(/^find/, function (next) {
     this.find({ secretTour: { $ne: true } });
+
+    this.start = Date.now();
+    next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+    console.log(`Query took ${Date.now() - this.start} milliseconds!`);
     next();
 });
 
